f4ca98fd7c4c3dc8b55e7be20cc391320dee9899
Hojin02 <140497846+Hojin02@users.noreply.github.com>
2025-01-27 07:08:09 +0900

    :recycle: refactor: Spring Security적용

--- src/main/java/org/example/expert/config/FilterConfig.java
+++ src/main/java/org/example/expert/config/FilterConfig.java
@@ -8,15 +8,16 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @RequiredArgsConstructor
 public class FilterConfig {
+    // 스프링 시큐리티가 있으므로 해당 클래스는 필요가 없음
 
-    private final JwtUtil jwtUtil;
-
-    @Bean
-    public FilterRegistrationBean<JwtFilter> jwtFilter() {
-        FilterRegistrationBean<JwtFilter> registrationBean = new FilterRegistrationBean<>();
-        registrationBean.setFilter(new JwtFilter(jwtUtil));
-        registrationBean.addUrlPatterns("/*"); // 필터를 적용할 URL 패턴을 지정합니다.
-
-        return registrationBean;
-    }
+//    private final JwtUtil jwtUtil;
+//
+//    @Bean
+//    public FilterRegistrationBean<JwtFilter> jwtFilter() {
+//        FilterRegistrationBean<JwtFilter> registrationBean = new FilterRegistrationBean<>();
+//        registrationBean.setFilter(new JwtFilter(jwtUtil));
+//        registrationBean.addUrlPatterns("/*"); // 필터를 적용할 URL 패턴을 지정합니다.
+//
+//        return registrationBean;
+//    }
 }
